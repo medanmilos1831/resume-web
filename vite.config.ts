ce87973e4f5e940d0a854209646fbd3d2f65d453
medanmilos1831 <medanmilos1831@gmail.com>
2024-09-10 12:21:20 +0200

    base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 export default defineConfig({
   root: __dirname,
   cacheDir: './node_modules/.vite/resume-web',
+  base: '/resume-web/',
 
   server: {
     port: 4200,
